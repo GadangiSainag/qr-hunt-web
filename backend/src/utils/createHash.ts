b8a7c015cfdbd7e464f57f6e1b8fe6c178226337
Sainag2k23 <saigadangi@gmail.com>
2024-10-08 00:16:17 +0530

    Make hashing more strong

--- backend/src/utils/createHash.ts
+++ backend/src/utils/createHash.ts
@@ -1,7 +1,8 @@
 const cryptolib = require("crypto");
 
-export default function createSHA256Hash(string: String, salt?: String  ) {
+export default function createSHA256Hash(string: String, salt?: String) {
   const hash = cryptolib.createHash("sha256");
   hash.update(string, salt);
+  hash.update(hash.toString(), salt);
   return hash.digest("hex");
 }
