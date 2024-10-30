387be27117c013e8b1c56f3b9130b725f1821d30
Sainag <saigadangi@gmail.com>
2024-10-30 17:32:03 +0530

    Beautify admin login page

--- frontend/src/Pages/Admin/Login.tsx
+++ frontend/src/Pages/Admin/Login.tsx
@@ -1,9 +1,20 @@
 import axios from "axios";
 import React, { useState } from "react";
 import style from "./login.module.css";
-
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/hooks";
+import { Button } from "@/Components/ui/button";
+import { Label } from "@/Components/ui/label";
+import { Input } from "@/Components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/Components/ui/card";
+
 const Login = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
@@ -39,27 +50,53 @@ const Login = () => {
 
   return (
     <div>
-      <h1 className={style.test}>Login</h1>
-      {isAuthenticated && role === 'admin' && "already logged in"}
-      <form onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input
-          required={true}
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <br />
-        <label>Password:</label>
-        <input
-          required={true}
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <br />
-        <button type="submit">Login!!</button>
-      </form>
+      <Card>
+        <CardHeader>
+          <CardTitle>Admin Login</CardTitle>
+          <CardDescription>
+            Only Admin knows a way to register himself😎
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              {/* <Label htmlFor="id-admin">Id</Label> */}
+              <br />
+              <Input
+                type="text"
+                value={userName}
+                id="id-admin"
+                onChange={(e) => setUserName(e.target.value)}
+                placeholder="Id"
+                required={true}
+              />
+            </div>
+
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+              <br />
+              {/* <Label htmlFor="password" >Password</Label> */}
+              <Input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required={true}
+              />
+            </div>
+            <br />
+            <Button type="submit">Log me in!</Button>
+          </form>
+            <br />
+            {isAuthenticated &&
+              role === "admin" &&
+              <p className="text-lime-300">You are already logged in 🔥</p>}
+        </CardContent>
+        <CardFooter>
+          <div>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
