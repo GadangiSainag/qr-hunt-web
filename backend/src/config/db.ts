e8271d5e286ec5ee654365dd17f48ee3d405870d
Sainag <saigadangi@gmail.com>
2025-04-13 16:16:14 +0530

    Hotfix: firebase credentials

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,15 +1,15 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import path from 'path';
+// import path from 'path';
 
 dotenv.config();
 
 // Path to your service account JSON file
-const serviceAccountPath = path.resolve(__dirname, '../../src/config/firebaseServiceAccount.json');
-
+// const serviceAccountPath = path.resolve(__dirname, '../../src/config/firebaseServiceAccount.json');
+const firebaseConfig = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || '{}');
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
+    credential: admin.credential.cert(firebaseConfig),
     databaseURL: process.env.FIREBASE_DATABASE_URL, // Firestore Database URL from Firebase Console
 });
 
