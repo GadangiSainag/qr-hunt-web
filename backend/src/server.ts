03f494f15ab358175efba2188774e400b735832e
Sainag <saigadangi@gmail.com>
2025-04-13 16:02:50 +0530

    Hotfix: Production grade variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import app from './app';
+const rawPort = process.env.PORT || '3000';
+const PORT = parseInt(rawPort, 10);
 
-
-
-const PORT = parseInt(process.env.SERVER_PORT as string) ;
+if (isNaN(PORT)) {
+    throw new Error(`Invalid PORT value: ${rawPort}`);
+}
 
 app.listen(PORT,  '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
